86199e6cf305f5050549ca33124509dadb89a6ab
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-20 00:14:22 +0600

    load data using the use effect and display data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <LoadPosts></LoadPosts>
       <District city="New York" country="USA"></District>
       <District city="Canada" country="North America"></District>
       <District city="Berlin" country="Germany"></District>
     </div>
   );
+}
+
+
+function LoadPosts(){
+  const [posts, setPosts] = useState([]);
+  useEffect( () =>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setPosts(data))
+  }, [])
+  return(
+    <div>
+      <h1>Posts: {posts.length}</h1>
+      {
+        posts.map(post => <Post title ={post.title} body={post.body} ></Post>)
+      }
+    </div>
+  )
+}
+
+function Post(props){
+  return(
+    <div style={{backgroundColor: 'lightgray', margin: '20px', border: '4px solid salmon'}}>
+      <h2>Title: {props.title}</h2>
+      <p>Body: {props.body}</p>
+    </div>
+  )
 }
 
 const districtStyle ={
